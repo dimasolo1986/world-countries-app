85d2024e1e5478e95e97dbe243e09b7ea270ecf5
Dmytro Solovei <dmytro.solovei@motorolasolutions.com>
2025-02-14 18:44:02 +0200

    Minor fix

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -59,11 +59,7 @@ class mapView {
       .fitWorld()
       .setView(latLon, defaultZoomLevel);
     L.tileLayer(
-      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}",
-      {
-        attribution:
-          "Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012",
-      }
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
     ).addTo(this._map);
     this.addDevelopmentPlaceMarker();
     this._addResetZoomToMap();
