544bfc46cee9babf33c9be77d6dd12df38ded19f
Dmytro Solovei <dmytro.solovei@motorolasolutions.com>
2025-03-20 16:10:47 +0200

    Minor fix

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -46,7 +46,11 @@ class mapView {
       "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
     );
     const siteliteLayer = L.tileLayer(
-      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      {
+        attribution:
+          "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+      }
     );
     const baseMaps = {
       Satellite: siteliteLayer,
