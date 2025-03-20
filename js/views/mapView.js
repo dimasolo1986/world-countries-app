2169fe6656b5e7517556055b167b0f6fd3ce8ad3
Dmytro Solovei <dmytro.solovei@motorolasolutions.com>
2025-03-20 16:07:14 +0200

    Minor fix

--- js/views/mapView.js
+++ js/views/mapView.js
@@ -46,10 +46,7 @@ class mapView {
       "https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
     );
     const siteliteLayer = L.tileLayer(
-      "https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}",
-      {
-        ext: "jpg",
-      }
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
     );
     const baseMaps = {
       Satellite: siteliteLayer,
